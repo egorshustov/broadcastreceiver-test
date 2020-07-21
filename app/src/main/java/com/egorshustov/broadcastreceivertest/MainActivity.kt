bc225eeb57c0a29d538be847120b1e51fd57f115
Egor Shustov <egorshustov.93@gmail.com>
2020-07-22 01:25:23 +0300

    Code refactored

--- app/src/main/java/com/egorshustov/broadcastreceivertest/MainActivity.kt
+++ app/src/main/java/com/egorshustov/broadcastreceivertest/MainActivity.kt
@@ -5,19 +5,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private val orderedReceiver1: OrderedReceiver1 = OrderedReceiver1()
+
+    private val orderedReceiver1 = OrderedReceiver1()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intentFilter = IntentFilter("com.egorshustov.EXAMPLE_ACTION")
-        intentFilter.priority = 1
+        val intentFilter = IntentFilter("com.egorshustov.EXAMPLE_ACTION").apply {
+            priority = 1
+        }
         registerReceiver(orderedReceiver1, intentFilter, android.Manifest.permission.VIBRATE, null)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(orderedReceiver1)
+    }
+
+    companion object {
+        const val STRING_EXTRA_KEY = "stringExtra"
     }
 }
--- app/src/main/java/com/egorshustov/broadcastreceivertest/OrderedReceiver2.kt
+++ app/src/main/java/com/egorshustov/broadcastreceivertest/OrderedReceiver2.kt
@@ -4,26 +4,28 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.egorshustov.broadcastreceivertest.MainActivity.Companion.STRING_EXTRA_KEY
 
 class OrderedReceiver2 : BroadcastReceiver() {
+
     override fun onReceive(context: Context?, intent: Intent?) {
         var resultCode = resultCode
         var resultData = resultData
         val resultExtras = getResultExtras(true)
-        var stringExtra = resultExtras?.getString("stringExtra")
+        var stringExtra = resultExtras?.getString(STRING_EXTRA_KEY)
 
         ++resultCode
         stringExtra += "->OR2"
 
         val toastText = "OR2\n" +
-                "resultCode: " + resultCode + "\n" +
-                "resultData: " + resultData + "\n" +
-                "stringExtra: " + stringExtra
+                "resultCode: $resultCode\n" +
+                "resultData: $resultData\n" +
+                "stringExtra: $stringExtra"
 
         Toast.makeText(context, toastText, Toast.LENGTH_LONG).show()
 
         resultData = "OR2"
-        resultExtras.putString("stringExtra", stringExtra)
+        resultExtras.putString(STRING_EXTRA_KEY, stringExtra)
 
         setResult(resultCode, resultData, resultExtras)
 
